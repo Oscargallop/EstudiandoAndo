e8bb4e5be9c522052eda94846178fad1f6f43cca
Oscargallop <oscargallop@gmail.com>
2024-07-22 13:10:18 -0500

    Version Final Juego Adivina el numero de Nezuco

---
+++ app.js
@@ -0,0 +1,83 @@
+//Definicion de variables
+let numeroSecreto = 0;
+let intentos = 0;
+let listaNumerosSorteados = [];
+//Programacion del numero maximo
+let numeroMaximo = 10;
+
+
+
+function asignarTextoElemento(elemento, texto) {
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento() {
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    // Verificar que el numero ingresado sea menor que el numero maximo
+    if (numeroDeUsuario < numeroMaximo) {
+        if (numeroDeUsuario === numeroSecreto) {
+            asignarTextoElemento('p',`Acertaste el número en ${intentos} ${(intentos === 1) ? 'vez' : 'veces'}`);
+            document.getElementById('reiniciar').removeAttribute('disabled');
+        } else {
+            //El usuario no acertó.
+            if (numeroDeUsuario > numeroSecreto) {
+                asignarTextoElemento('p','Nezuco dice que el número es menor');
+            } else {
+             asignarTextoElemento('p','Nezuco dice que el número es mayor');
+            }
+            intentos++;
+            limpiarCaja();
+        }
+    } else {
+        alert(`Nezuco va a llamar a Tanjiro!!!!, te dijo que era hasta ${numeroMaximo}`)
+    }
+
+    return;
+}
+
+function limpiarCaja() {
+    document.querySelector('#valorUsuario').value = '';
+}
+
+function generarNumeroSecreto() {
+    let numeroGenerado =  Math.floor(Math.random()*numeroMaximo)+1;
+
+    console.log(numeroGenerado);
+    console.log(listaNumerosSorteados);
+    //Si ya sorteamos todos los números
+    if (listaNumerosSorteados.length == numeroMaximo) {
+        asignarTextoElemento('p',`Ya Nezuco se durmio, esta cansada porque ha jugado ${numeroMaximo} veces`);
+    } else {
+        //Si el numero generado está incluido en la lista 
+        if (listaNumerosSorteados.includes(numeroGenerado)) {
+            return generarNumeroSecreto();
+        } else {
+            listaNumerosSorteados.push(numeroGenerado);
+            return numeroGenerado;
+        }
+    }
+}
+
+function condicionesIniciales() {
+    asignarTextoElemento('h1','Adivina el numero de Nezuco');
+    asignarTextoElemento('p',`Di un número del 1 al ${numeroMaximo}`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+    console.log(numeroSecreto);
+}
+
+function reiniciarJuego() {
+    //limpiar caja
+    limpiarCaja();
+    //Indicar mensaje de intervalo de números 
+    //Generar el número aleatorio
+    //Inicializar el número intentos
+    condicionesIniciales();
+    //Deshabilitar el botón de nuevo juego
+    document.querySelector('#reiniciar').setAttribute('disabled','true');
+    
+}
+
+condicionesIniciales();
